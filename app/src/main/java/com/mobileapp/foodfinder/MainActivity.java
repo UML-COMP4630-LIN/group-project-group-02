f97724bc9e9d33a030f0713e6c10c996071442ed
andrewjacobson5 <andrewjacobson12@outlook.com>
2024-12-06 16:46:29 -0500

    Made it green, added drawer, removed settings

--- app/src/main/java/com/mobileapp/foodfinder/MainActivity.java
+++ app/src/main/java/com/mobileapp/foodfinder/MainActivity.java
@@ -1,21 +1,26 @@
 package com.mobileapp.foodfinder;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.mobileapp.foodfinder.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +28,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_settings, R.id.navigation_favorites)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navDrawer = findViewById(R.id.nav_drawer);
+
+        // Set up the drawer toggle
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Handle navigation item clicks
+        navDrawer.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId(); // Store the ID to debug issues
+            if (id == R.id.menu_dark_mode) {
+                Toast.makeText(this, "Dark Mode Toggled", Toast.LENGTH_SHORT).show();
+                // Implement dark mode logic here
+            } else if (id == R.id.menu_about) {
+                Toast.makeText(this, "About Section", Toast.LENGTH_SHORT).show();
+                // Open About dialog or navigate to About fragment
+            }
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
